7b5e45b6ba8f4aae6349a52727ab26ed2269afb9
Ronald <ronalddavisr3@gmail.com>
2021-12-01 01:08:17 -0500

    removed some alembic revisions

--- alembic_languages_db/versions/6e023f348f69_finish_all_tbl.py
+++ alembic_languages_db/versions/6e023f348f69_finish_all_tbl.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '6e023f348f69'
-down_revision = '4a7e393bedee'
+down_revision = None
 branch_labels = None
 depends_on = None
 
