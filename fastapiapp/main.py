7e67a3d73c4d3c83e7b8546804c313c318a4fdaf
Ronald <ronalddavisr3@gmail.com>
2021-12-03 20:15:15 -0500

    changing sqlalchemy setup

--- fastapiapp/main.py
+++ fastapiapp/main.py
@@ -5,11 +5,22 @@ from .routers import users, languages, auth, spokenlanguages
 
 from .db import engine
 from . import models
-
+from fastapi.middleware.cors import CORSMiddleware
 # models.Base.metadata.create_all(bind=engine)
 
-
+origins =[
+    "*"
+]
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(spokenlanguages.router)
 app.include_router(users.router)
@@ -19,3 +30,5 @@ app.include_router(auth.router)
 # @app.get("/sqlalchemytest")v
 # def test(db: Session = Depends(get_db)):
 #     print({'msg':'seems to work'})
+
+
